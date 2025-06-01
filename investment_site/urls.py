39478eaa74110616e521097d6f53f9d897e8a870
Nasim <nasim.ie.86@gmail.com>
2025-06-01 20:41:57 +0330

    Add admin route to urlpatterns

--- investment_site/urls.py
+++ investment_site/urls.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
+from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("سلام! این صفحه اصلی پروژه شماست.")
 
 urlpatterns = [
-    path('', home),
+    path('', home), # صفحه اصلی
+    path('admin/', admin.site.urls), # مسیر پنل مدیریت
 ]
