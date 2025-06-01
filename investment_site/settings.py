07d876e6cfdb4d1e6fc824e88ed09595e1830657
Nasim <nasim.ie.86@gmail.com>
2025-06-01 21:54:57 +0330

    Configure WhiteNoise for static files

--- investment_site/settings.py
+++ investment_site/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
 
     'django.middleware.security.SecurityMiddleware',
 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
 
     'django.middleware.common.CommonMiddleware',
@@ -170,3 +172,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
